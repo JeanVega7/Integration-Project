760bc7d88e578483615542b7fe9b322e895bf331
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-10-19 19:09:05 -0400

    Module 1 code

--- src/Mod01Hello.java
+++ src/Mod01Hello.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -7,6 +9,51 @@
  *
  */
 public class Mod01Hello {
-    
 
-}
+        public static void Welcome() {
+            System.out.println("Module 1");
+            System.out.println("Hello, World.");
+            System.out.println("Hello, Java.");
+        }
+        public static void Day0() {
+            // Create a Scanner object to read input from stdin.
+            Scanner scan = new Scanner(System.in); 
+            
+            // Read a full line of input from stdin and save it to our variable, inputString.
+            String inputString = scan.nextLine(); 
+
+            // Close the scanner object, because we've finished reading 
+                // all of the input from stdin needed for this challenge.
+            scan.close(); 
+              
+            // Print a string literal saying "Hello, World." to stdout.
+            System.out.println("Hello, World.");
+            System.out.println(inputString);
+              
+          }
+        static int solveMeFirst(int a, int b) {
+            return a+b;
+        }
+
+       public static void solveFirst() {
+              Scanner in = new Scanner(System.in);
+              int a;
+              a = in.nextInt();
+              int b;
+              b = in.nextInt();
+              int sum;
+              sum = solveMeFirst(a, b);
+              System.out.println(sum);
+        }
+       public static void javaStdin() {
+           Scanner scan = new Scanner(System.in);
+           int a = scan.nextInt();
+           int b = scan.nextInt();
+           int c = scan.nextInt();
+           System.out.println(a);
+           System.out.println(b);
+           System.out.println(c);
+       }
+       
+}     
+
