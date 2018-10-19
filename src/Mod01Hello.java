a429987bf3c31a091737c5239c54e40b0eacff98
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-10-19 19:18:08 -0400

    Added user greeting

--- src/Mod01Hello.java
+++ src/Mod01Hello.java
@@ -13,29 +13,28 @@ public class Mod01Hello {
         public static void Welcome() {
             System.out.println("Module 1");
             System.out.println("Hello, World.");
-            System.out.println("Hello, Java.");
+            System.out.println("Hello, Java.");    
         }
+ //The following are the exercises from Hackerrank Module 1
         public static void Day0() {
             // Create a Scanner object to read input from stdin.
             Scanner scan = new Scanner(System.in); 
             
             // Read a full line of input from stdin and save it to our variable, inputString.
-            String inputString = scan.nextLine(); 
+            String usersName = scan.nextLine(); 
 
             // Close the scanner object, because we've finished reading 
                 // all of the input from stdin needed for this challenge.
             scan.close(); 
               
             // Print a string literal saying "Hello, World." to stdout.
-            System.out.println("Hello, World.");
-            System.out.println(inputString);
-              
+            System.out.println("Hello " + usersName);             
           }
+        
         static int solveMeFirst(int a, int b) {
             return a+b;
         }
-
-       public static void solveFirst() {
+        public static void solveFirst() {
               Scanner in = new Scanner(System.in);
               int a;
               a = in.nextInt();
@@ -45,6 +44,7 @@ public class Mod01Hello {
               sum = solveMeFirst(a, b);
               System.out.println(sum);
         }
+       
        public static void javaStdin() {
            Scanner scan = new Scanner(System.in);
            int a = scan.nextInt();
