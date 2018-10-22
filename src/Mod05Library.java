c3377e4e1979c57f6108a612e303727c5929a055
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-10-21 22:42:37 -0400

    mod05 finished

--- src/Mod05Library.java
+++ src/Mod05Library.java
@@ -1,11 +1,29 @@
-/**
- * 
- */
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- * @author jcvega5237
- *
- */
 public class Mod05Library {
+    public static void randomGen() {
+     //simulate lotto drawing of 6 random numbers   
+        Random r = new Random();
+        System.out.println(r.nextInt(53)+1);
+        System.out.println(r.nextInt(53)+1);
+        System.out.println(r.nextInt(53)+1);
+        System.out.println(r.nextInt(53)+1);
+        System.out.println(r.nextInt(53)+1);
+        System.out.println(r.nextInt(53)+1);
+    }
+    
+    public static void pizzaSlice() {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Tell me the size of your slice and I will tell"); 
+        System.out.println("you which is larger, the number entered, or PI");
+        
+        double slice = scan.nextDouble();
+        scan.close();
+        
+        System.out.println(Math.max(slice,Math.PI));
+    }
 
 }
