deef62fe978a3b711132b5d2d8f6bc7490d31400
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-10-21 22:04:50 -0400

    mod04 finished

--- src/Mod04Methods.java
+++ src/Mod04Methods.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Mod04Methods {
     public static void doMod04() {
         
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Print integer: ");
+        Double integer = scan.nextDouble();
+        
+        scan.close();
+        
+        /* In Square(integer), Square() would be considered the method call
+         * while (integer) would be the argument
+         */
+        System.out.println("Square: "+ Square(integer)+ "\nCube: "+ Cube(integer));
+    }
+    
+    /* In public static Double Square(double sq),
+     * public static Double Square() would be the header while
+     * double sq is the parameter
+     */
+    public static Double Square(double sq) {
+        
+        Double SquareRoot = sq * sq;
+        return SquareRoot;
+    }
+    public static Double Cube(double cu) {
+        
+        Double CubeRoot = cu * cu * cu;
+        return CubeRoot;
     }
 
 }
