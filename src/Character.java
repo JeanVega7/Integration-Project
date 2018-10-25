3e6ca3ecb834d44fc8895b5a72e90c0ea9690a19
jcvega5237 <jcvega5237@7127R3S4.STUDENT.ad.fgcu.edu>
2018-10-25 09:54:03 -0400

    Character class

---
+++ src/Character.java
@@ -0,0 +1,20 @@
+
+public class Character {
+    
+    // Fields (almost always private)
+    private int score;
+    
+    public Character(int sc) {
+        score = sc;
+    }
+    public Character() {
+        score = 0;
+    }
+    public void setScore(int sc) {
+        score = sc;
+    }
+    public int getScore() {
+        return score;
+    }
+
+}
