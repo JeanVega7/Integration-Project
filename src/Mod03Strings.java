79fc75b1d238a1a5b58303fadbb42aefff3113bb
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-12-17 08:10:03 -0500

    mod03 checlstyle

--- src/Mod03Strings.java
+++ src/Mod03Strings.java
@@ -5,47 +5,49 @@ import java.util.Scanner;
  *
  */
 public class Mod03Strings {
-    public static void stringsIntro() {
-        System.out.println("Module 3" + "\n");
+  public static void stringsIntro() {
+    System.out.println("Module 3" + "\n");
         
-        System.out.println("Enter the name of two animals in lower case");
+    System.out.println("Enter the name of two animals in lower case");
         
-        Scanner scan = new Scanner(System.in);
-        String AnimalName = scan.next();
-        String SecondAnimal = scan.next();
+    Scanner scan = new Scanner(System.in);
+    String animalName = scan.next();
+    String secondAnimal = scan.next();
+     
+    scan.close();
         
-        scan.close();
+    
+    //.length counts the number of characters in a string
+    System.out.println("The first animal's name is " + animalName.length() + " letters long");
+    System.out.println("The second animal's name is " + secondAnimal.length() + " letters long");
         
-        //.length counts the number of characters in a string
-        System.out.println("The first animal's name is "+ AnimalName.length() + " letters long");
-        System.out.println("The second animal's name is "+ SecondAnimal.length() + " letters long");
+    System.out.println("Does the name of the first animal appear "
+            + "before the second animal in alphabetical order?");
         
-        System.out.println("Does the name of the first animal appear before the second animal in alphabetical order?");
+    //.compareTo compares strings to find alphabetical order
+    int charNum = animalName.compareTo(secondAnimal);
+    if (charNum > 0) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
         
-        //.compareTo compares strings to find alphabetical order
-        int charNum = AnimalName.compareTo(SecondAnimal);
-        if(charNum>0){
-             System.out.println("Yes");
-        } else{
-             System.out.println("No");
-        }
-        
-        //.substring addresses a specific portion of the String 
-        //.toUpperCase changes characters to an upper case
-        // The \n escape sequence inserts another line
-        String firstCap = AnimalName.substring(0,1).toUpperCase()+AnimalName.substring(1);
-        String secondCap = SecondAnimal.substring(0,1).toUpperCase()+SecondAnimal.substring(1);;
-        System.out.print("The first animal is the " + firstCap + ".\n" 
+    //.substring addresses a specific portion of the String 
+    //.toUpperCase changes characters to an upper case
+    // The \n escape sequence inserts another line
+    String firstCap = animalName.substring(0,1).toUpperCase() + animalName.substring(1);
+    String secondCap = secondAnimal.substring(0,1).toUpperCase() + secondAnimal.substring(1);
+    System.out.print("The first animal is the " + firstCap + ".\n" 
                 + "The second animal is the " + secondCap + ".");
         
-    }
+  }
     
-    public static void castingEx() {
-        // casting changes data from one data type to another
-        int num1 = 53;
-        int num2 = 47;
-        byte num3;
-        num3 = (byte)(num1 + num2);
-        System.out.println(num3);
-    }
+  public static void castingEx() {
+    // casting changes data from one data type to another
+    int num1 = 53;
+    int num2 = 47;
+    byte num3;
+    num3 = (byte)(num1 + num2);
+    System.out.println(num3);
+  }
 }
