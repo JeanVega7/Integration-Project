a8a1d4810d2735333254ffc0d9e1df17b295dd4b
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-12-17 02:08:12 -0500

    mod08 comment

--- src/Mod08Classes.java
+++ src/Mod08Classes.java
@@ -1,4 +1,8 @@
 
 public class Mod08Classes {
-
+    /*
+     * Character class and CollegeClass Class
+     * contain the private fields, public getters and setters
+     * as well as the constructors with and without parameters
+     */
 }
