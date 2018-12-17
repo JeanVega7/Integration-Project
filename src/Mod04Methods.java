29f348b8c93b75a2a6df7a33ccada72fc20fd6f3
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-12-17 08:12:46 -0500

    mod04 checkstyle

--- src/Mod04Methods.java
+++ src/Mod04Methods.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
  *
  */
 public class Mod04Methods {
-    public static void doMod04() {
-        System.out.println("Module 4" + "\n");
+  public static void doMod04() {
+    System.out.println("Module 4" + "\n");
         
-        System.out.println("Provide a number to calculate the square and cube of it");
+    System.out.println("Provide a number to calculate the square and cube of it");
         
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Print integer: ");
-        Double integer = scan.nextDouble();
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Print integer: ");
+    Double integer = scan.nextDouble();
         
-        scan.close();
+    scan.close();
         
-        /* In Square(integer), Square() would be considered the method call
-         * while (integer) would be the argument
-         */
-        System.out.println("Square: "+ Square(integer)+ "\nCube: "+ Cube(integer));
-    }
+    /* In Square(integer), Square() would be considered the method call
+    * while (integer) would be the argument
+    */
+    System.out.println("Square: " + square(integer) + "\nCube: " + cube(integer));
+  }
     
     /* In public static Double Square(double sq),
      * public static Double Square() would be the header while
      * double sq is the parameter
      */
-    public static Double Square(double sq) {
+    public static Double square(double sq) {
         
-        Double squareRoot = sq * sq;
-        return squareRoot;
-    }
-    public static Double Cube(double cu) {
+    Double squareRoot = sq * sq;
+    return squareRoot;
+  }
+    public static Double cube(double cu) {
         
-        Double cubeRoot = cu * cu * cu;
-        return cubeRoot;
-    }
+    Double cubeRoot = cu * cu * cu;
+    return cubeRoot;
+  }
 
 }
