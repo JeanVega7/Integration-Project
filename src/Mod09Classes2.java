d0ecffe7377c6c9b6978a3d0ca9c43ce35e55019
JeanVega7 <35906883+JeanVega7@users.noreply.github.com>
2018-12-17 03:39:58 -0500

    mod09 complete plus animal, dog, cat class

--- src/Mod09Classes2.java
+++ src/Mod09Classes2.java
@@ -1,4 +1,46 @@
 
 public class Mod09Classes2 {
+    
+    public static void superInherit() {
+       abstract class Book {
+        String title;
+        String author;
 
+        Book(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
+        abstract void display();
+        
+    }
+        class MyBook extends Book {
+            int  price;  
+            public MyBook(String title, String author,int price) {
+                super(title,author);
+                this.price = price;
+        }
+        public void display() { 
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("Price: " + price);               
+        }
+        }
+       /*
+        * Some of the benefits of inheritance include
+        * ease of field or method alteration as well as 
+        * reuse for easier writing of code
+        */
+    }
+   public static void polyClass() {
+        Animal[] thelist = new Animal[1];
+        Dog d = new Dog();
+        Cat c = new Cat();
+        
+        thelist[0] = d;
+        thelist[1] = c;
+        
+        for(Animal x: thelist) {
+            x.noise();
+        }
+    }
 }
